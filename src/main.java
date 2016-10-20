4d8f81377fa328a82c8a53714db0d7789c275157
Toomas Koost <toomas@wizibe.com>
2016-10-20 21:56:53 +0300

    parandasin GUId

--- src/main.java
+++ src/main.java
@@ -4,23 +4,46 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
-
 import javafx.stage.Stage;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 //pohiliides
 public class main extends Application {
+
+
+
+    public static void createNewDatabase(String fileName) {
+        fileName = new String("OIS.db");
+        String url = "jdbc:sqlite:/home/Projects/OIS/db/" + fileName;
+
+        try (Connection conn = DriverManager.getConnection(url)) {
+            if (conn != null) {
+                DatabaseMetaData meta = conn.getMetaData();
+                System.out.println("The driver name is " + meta.getDriverName());
+                System.out.println("A new database has been created.");
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("ÕLLEINFOSÜSTEEM");
 
-        Button ostaButton = new Button("OSTA");
-        Button inimButton = new Button("LISA INIMESI");
-        Button laduButton = new Button("LADU");
 
-        HBox menyy = new HBox(0, ostaButton, inimButton, laduButton);
-        menyy.setAlignment(Pos.TOP_CENTER);
+        Button ostaButton = new Button("OSTA ÕLUT");
+        Button inimButton = new Button("LISA INIMESI");
+        Button laduButton = new Button("LADU (VALVE / MAJAVANEM)");
+
+        HBox menyy = new HBox(0);
+        menyy.getChildren().addAll(ostaButton, inimButton, laduButton);
+        menyy.setAlignment(Pos.TOP_LEFT);
 
         StackPane oisMain = new StackPane();
         oisMain.getChildren().addAll(menyy);
